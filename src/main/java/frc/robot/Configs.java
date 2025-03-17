b8b1add6da6f08ae6425c61263811e3249884187
KillerDD13 <KillerDD013@gmail.com>
2025-03-17 07:33:35 -0500

    Configs.java changes

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -5,11 +5,22 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public final class Configs {
     
-    public static final class CoralFunnel {
+    public static final class SparkConfigs {
         public static final SparkMaxConfig FUNNEL_CONFIG = new SparkMaxConfig();
+        public static final SparkMaxConfig L_ELEV_CONFIG = new SparkMaxConfig();
+        public static final SparkMaxConfig R_ELEV_CONFIG = new SparkMaxConfig();
+        public static final SparkMaxConfig PIVOT_CONFIG = new SparkMaxConfig();
+        public static final SparkMaxConfig ALGAE_CONFIG = new SparkMaxConfig();
 
         static {
-            FUNNEL_CONFIG.idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+            FUNNEL_CONFIG.inverted(false).idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+
+            L_ELEV_CONFIG.inverted(false).idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+            R_ELEV_CONFIG.inverted(true).idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+
+            PIVOT_CONFIG.inverted(false).idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+
+            ALGAE_CONFIG.inverted(false).idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
         }
 
     }
